e77d8d4cd9b2e6879df884c9e13fef3001695fe2
ShiriezH <shiriezhendriks1@gmail.com>
2026-05-31 17:43:50 +0200

    Implement request line parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,63 @@
 package request
+
+import (
+	"errors"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(data), "\r\n")
+
+	requestLine, err := parseRequestLine(lines[0])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: requestLine,
+	}, nil
+}
+
+func parseRequestLine(line string) (RequestLine, error) {
+	parts := strings.Split(line, " ")
+
+	if len(parts) != 3 {
+		return RequestLine{}, errors.New("invalid request line")
+	}
+
+	method := parts[0]
+	target := parts[1]
+	version := parts[2]
+
+	methodRegex := regexp.MustCompile(`^[A-Z]+$`)
+	if !methodRegex.MatchString(method) {
+		return RequestLine{}, errors.New("invalid method")
+	}
+
+	if version != "HTTP/1.1" {
+		return RequestLine{}, errors.New("invalid version")
+	}
+
+	return RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   "1.1",
+	}, nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,57 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	// Good GET Request line
+	r, err := RequestFromReader(strings.NewReader(
+		"GET / HTTP/1.1\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Good GET Request line with path
+	r, err = RequestFromReader(strings.NewReader(
+		"GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Good POST Request with path
+	r, err = RequestFromReader(strings.NewReader(
+		"POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+
+	// Invalid number of parts
+	_, err = RequestFromReader(strings.NewReader(
+		"/coffee HTTP/1.1\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.Error(t, err)
+
+	// Invalid method
+	_, err = RequestFromReader(strings.NewReader(
+		"GeT /coffee HTTP/1.1\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.Error(t, err)
+
+	// Invalid version
+	_, err = RequestFromReader(strings.NewReader(
+		"GET /coffee HTTP/2.0\r\nHost: localhost:42069\r\n\r\n",
+	))
+	require.Error(t, err)
 }
